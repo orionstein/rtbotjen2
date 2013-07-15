f6f24c987b4f0ccc5db83dac9c7b731a7a631af3
Mark Hoffmeyer <mhoffmeyer@gmail.com>
2013-07-15 14:52:52 -0400

    Cleanup!

--- plugins/lunch.rb
+++ plugins/lunch.rb
@@ -5,15 +5,14 @@ module Cinch
     class Lunch
 	    include Cinch::Plugin
 
-	    match /lunch:(.*)/i,	method: :lunchtime
-	    match /lunch (.*)/i,	method: :lunchtime
+	    match /lunch/i,	method: :lunchtime
 
 	    def initialize(*args)
         	super
         	places = ["TJ's","Bangkok Joe's","Tackle Box","Café Cantina","Farmers Fishers Bakers","Moby Dick","Johnny Rockets","Tbsp","Muncheez Mania","Old Glory","Chipotle","Shophouse","Café Tu-o-Tu","Georgetown Dinette","Cheap Chinese Place"]
 	    end
 
-	    def lunchtime(m,wolfy)
+	    def lunchtime()
 	    	index = 1 + rand(places.length)
 		    m.reply places[index]
 	    end
