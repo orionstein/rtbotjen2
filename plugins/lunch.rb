92db133761687e890b9b94d9c2b93110520e22c2
Mark Hoffmeyer <mhoffmeyer@gmail.com>
2013-07-15 14:43:25 -0400

    Initial Commit for Lunchtime Plugin

---
+++ plugins/lunch.rb
@@ -0,0 +1,24 @@
+require 'cinch'
+
+module Cinch
+  module Plugins
+    class Lunch
+	    include Cinch::Plugin
+
+	    match /lunch:(.*)/i,	method: :lunchtime
+	    match /lunch (.*)/i,	method: :lunchtime
+
+	    def initialize(*args)
+        	super
+        	places = ["TJ's","Bangkok Joe's","Tackle Box","Café Cantina","Farmers Fishers Bakers","Moby Dick","Johnny Rockets","Tbsp","Muncheez Mania","Old Glory","Chipotle","Shophouse","Café Tu-o-Tu","Georgetown Dinette","Cheap Chinese Place"]
+	    end
+
+	    def lunchtime(m,wolfy)
+	    	index = 1 + rand(places.length)
+		    m.reply places[index]
+	    end
+    end
+  end
+end
+
+
