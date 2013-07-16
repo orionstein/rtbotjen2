cf959e594029eb2ac90848452aed1fcdc84924c6
Mark Hoffmeyer <mhoffmeyer@gmail.com>
2013-07-16 09:25:01 -0400

    Needs message as arg

--- plugins/lunch.rb
+++ plugins/lunch.rb
@@ -12,7 +12,7 @@ module Cinch
         	places = ["TJ's","Bangkok Joe's","Tackle Box","Cafe Cantina","Farmers Fishers Bakers","Moby Dick","Johnny Rockets","Tbsp","Muncheez Mania","Old Glory","Chipotle","Shophouse","Cafe Tu-o-Tu","Georgetown Dinette","Cheap Chinese Place"]
 	    end
 
-	    def lunchtime()
+	    def lunchtime(m)
 		    m.reply places.sample
 	    end
     end
